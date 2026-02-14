7d59eaf32312ceabdc36995cf0b121bd01f6b301
Cristinabreu <cristinabreu2018@gmail.com>
2026-02-14 16:37:36 -0300

    inicializacao HTML

---
+++ products.js
@@ -0,0 +1,9 @@
+const menuOptions = [
+    { name: "x-Salada", price: 30, vegan: false, src: "assets/x-salada.jpg" },
+    { name: "x-Bacon", price: 34, vegan: false, src: "assets/x-bacon.jpg" },
+    { name: "x-Bacon Egg", price: 39, vegan: false, src: "assets/x-bacon-egg.jpg" },
+    { name: "Monstruoso", price: 50, vegan: false, src: "assets/x-burger.jpg" },
+    { name: "Big Vegano", price: 55, vegan: true, src: "assets/x-vegano.jpg" },
+    { name: "x-Vegan", price: 45, vegan: true, src: "assets/x-vegano-supreme.jpg" }
+
+]
