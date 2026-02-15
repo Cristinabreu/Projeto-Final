2b473c08a0c4fedb08fc9a8772e9bf4b3e1a8535
Cristinabreu <cristinabreu2018@gmail.com>
2026-02-15 15:54:29 -0300

    Reduce Preço total

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".show-all");
 const buttonNewPrice = document.querySelector(".new-price");
+const buttonSumAll = document.querySelector(".sum-all");
+const filter = document.querySelector(".filter");
 
 
 function showAll(productArray) {
@@ -26,5 +28,17 @@ function porcentage10() {
     showAll(currentprice)
 }
 
+function sumAllPrices() {
+    myLi = ""
+    const sum = menuOptions.reduce((acc, curr) => acc + curr.price, 0)
+    myLi += `
+        <li>
+            <p> A soma de todos os lanches é de R$ ${sum.toFixed(2)}</p>
+        </li>
+    `
+    list.innerHTML = myLi
+}
+
 buttonShowAll.addEventListener("click", () => showAll(menuOptions))
 buttonNewPrice.addEventListener("click", porcentage10)
+buttonSumAll.addEventListener("click", sumAllPrices)
