7792fadcc810c415e8a57a5766ca1418045fb76e
Cristinabreu <cristinabreu2018@gmail.com>
2026-02-15 14:20:30 -0300

    Map. 10% de desconto

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const list = document.querySelector("ul");
 const buttonShowAll = document.querySelector(".show-all");
-let myLi = ""
+const buttonNewPrice = document.querySelector(".new-price");
 
-function showAll() {
-    menuOptions.forEach((option) => {
+
+function showAll(productArray) {
+    let myLi = ""
+    productArray.forEach((option) => {
         myLi += `
         <li>
             <img src=${option.src} alt="${option.name}">
                 <p>${option.name}</p>
-                <p class="item-price">R$ ${option.price},00</p>
+                <p class="item-price">R$ ${option.price.toFixed(2)}</p>
         </li>
     `
     })
     list.innerHTML = myLi
 }
 
+function porcentage10() {
+    const currentprice = menuOptions.map((product) => ({
+        ...product,
+        price: product.price * 0.9,
 
+    }))
+    showAll(currentprice)
+}
 
-
-buttonShowAll.addEventListener("click", showAll)
+buttonShowAll.addEventListener("click", () => showAll(menuOptions))
+buttonNewPrice.addEventListener("click", porcentage10)
